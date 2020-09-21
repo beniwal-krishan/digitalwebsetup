7f6078859d7286095624675dd634d68a020062aa
Krishan <beniwal.krishn@gmail.com>
2020-09-21 17:43:46 +0530

    done

--- config/database.js
+++ config/database.js
@@ -3,3 +3,10 @@
     mongodb+srv://beniwalkk:Userkk123@clusterkk.7ecvb.mongodb.net/<dbname>?retryWrites=true&w=majority;
 
 }
+
+
+module.exports = {
+
+    mongoDbUrl: "mongodb+srv://clusterkk.7ecvb.mongodb.net/<cms>" --username beniwalkk
+
+};
